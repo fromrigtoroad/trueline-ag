2324e06aa6ed723c890ccb94009e12fc33c782a2
Allan Melo <allan@Allans-MacBook-Air.local>
2026-06-27 22:05:06 -0400

    Quit Electron app when all windows are closed on macOS to prevent zombie Python processes

--- main.js
+++ main.js
@@ -309,9 +309,7 @@ app.whenReady().then(() => {
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  app.quit();
 });
 
 app.on('will-quit', () => {
